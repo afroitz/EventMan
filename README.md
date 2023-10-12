02d9061d1f7061466aba45036ef1a542cec62eac
Alex Froitzheim <froitzheim.alex@gmail.com>
2023-10-12 14:06:06 +0200

    Moved styles to public assets directory

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # EventMan
 A groundbreaking event management system created by:
-Andre Winzen 6062288
-Alex Froitzheim 7340859
+- Andre Winzen 6062288
+- Alex Froitzheim 7340859
 
--- index.js
+++ index.js
@@ -28,6 +28,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src/views'));
 
+app.use(express.static('src/public'));
+
 const PORT = process.env.PORT
 
 app.use('/', router);
