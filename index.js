6789f2f85451836fc99467677c1a1b3c59cdba2c
Andre Winzen <awinzen3@smail.uni-koeln.de>
2023-10-14 18:27:34 +0200

    added login view

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ import router from './src/routes/router.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import bodyParser from 'body-parser'; 
-
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
